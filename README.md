f14af7287dca5bd15fa383bb2bd9a4ecdffb1e61
Caffreyfans <Caffreyfans@163.com>
2019-03-05 18:48:03 +0800

    api 改用 v3 版

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 程序可能存在 BUG,如有任何问题请提出我会在第一时间解决。
     
+![演示](https://raw.githubusercontent.com/Caffreyfans/baidu_identity/master/gif/demo.gif)
+
 ## 使用
 下载插件, 并将 baidu_face.py 放置于 custom_components/sensor 文件夹下
 
@@ -9,21 +11,27 @@
 ```YAML
 sensor:
   - platform : baidu_face
-    # 必须项
-    #从百度ai开放平台人脸识别应用中获取
     api_key : "*************"
     secret_key: "***********"
-    group_id: "*************"
-    #摄像头entity_id
+    group_list: "['myself', 'family']"
     camera_entity_id: "*****"    
-    # token
     token: "****************"
-    #刷新间隔(1~30s)
-    scan_interval: 2
-    # 可选项
-    # name: "***************"
+    # liveness: "NORMAL"
+    # name: "ren lian shi bie"
     # port: 8123
     # pic_url: "网络、本地图片地址"
+    scan_interval: 2
 ```
 
-![演示](https://raw.githubusercontent.com/Caffreyfans/baidu_identity/master/gif/demo.gif)
+| 参数 | 必选 | 类型 | 说明 |
+|-|-|-|
+| api_key | 是 | string | 百度人脸识别应用 API Key |
+| secret_key | 是 | string | 百度人脸识别应用 Secret Key |
+| group_list | 是 | string | 人脸库用户组组名 |
+| camera_entity_id | 是 | string | ha 中摄像头实体 id |
+| token | 是 | string | ha 中永久令牌 token |
+| scan_interval | 是 | int | 每次检测检测时间<br> 建议 2|
+| liveness | 否 | string | 活体检测控制<br>NONE: 不进行控制<br>LOW:较低的活体要求(高通过率 低攻击拒绝率)<br>NORMAL: 一般的活体要求(平衡的攻击拒绝率, 通过率)<br>HIGH: 较高的活体要求(高攻击拒绝率 低通过率)<br>默认NONE 若活体检测结果不满足要求，则返回结果中会提示活体检测失败 |
+| name | 否 | string | 该实体名<br># 默认 "ren lian shi bie"|
+| port | 否 | int | ha 设定的端口号<br># 默认 8123|
+| pic_url | 否 | string | 识别失败时显示的图片<br># 默认 魔方gif图 |
--- custom_components/sensor/baidu_face.py
+++ custom_components/sensor/baidu_face.py
@@ -11,137 +11,130 @@ import base64
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_SCORES = 'scores'
 ATTR_GROUP_ID = 'group_id'
-ATTR_UID = 'uid'
+ATTR_SCORE = 'score'
+ATTR_UID = 'user_id'
 ATTR_USER_INFO = 'user_info'
-ATTR_FACELIVENESS = "faceliveness"
 
-CONF_NAME = 'name'
+ATTR_LIST = {
+	ATTR_GROUP_ID : "null",
+	ATTR_SCORE : "null",
+	ATTR_UID : "null",
+	ATTR_USER_INFO : "null"
+}
+
 CONF_APIKEY = 'api_key'
-CONF_SECRETKEY = 'secret_key'
-CONF_GROUP_ID = 'group_id'
-CONF_PORT = 'port'
 CONF_CAMERA_ENTITY_ID = 'camera_entity_id'
+CONF_GROUP_LIST = 'group_list'
+CONF_LIVENESS = 'liveness'
+CONF_NAME = 'name'
+CONF_PORT = 'port'
 CONF_PIC_URL = 'pic_url'
+CONF_SECRETKEY = 'secret_key'
 CONF_TOKEN = 'token'
 
 
-PASS_SCORE = 80
-THRESHOLD = 0.393241
+
+
+
 DEFAULT_NAME = "ren lian shi bie"
-DEFAULT_PIC_URL = "https://dev.tencent.com/u/Caffreyfans/p/public-sources/git/raw/master/1.gif"
+DEFAULT_PIC_URL = 'https://dev.tencent.com/u/Caffreyfans/p/public-sources/git/raw/master/1.gif'
 DEFAULT_PORT = 8123
+DEFAULT_LIVENESS = 'NORMAL'
+
+LOCAL_PATH = '/local/images/'
+PASS_SCORE = 80.0
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CONF_APIKEY): cv.string,
-    vol.Required(CONF_SECRETKEY): cv.string,
-    vol.Required(CONF_GROUP_ID): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
     vol.Required(CONF_CAMERA_ENTITY_ID): cv.string,
+    vol.Required(CONF_GROUP_LIST): cv.string,
+    vol.Optional(CONF_LIVENESS, default=DEFAULT_LIVENESS): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
     vol.Optional(CONF_PIC_URL, default=DEFAULT_PIC_URL): cv.string,
-    vol.Required(CONF_TOKEN): cv.string
+    vol.Required(CONF_SECRETKEY): cv.string,
+    vol.Required(CONF_TOKEN): cv.string,
 })
 
 
 def setup_platform(hass, config, add_devices,
 	               discovery_info=None):
     """ add sensor components """
-    name = config.get(CONF_NAME)
     api_key = config.get(CONF_APIKEY)
-    secret_key = config.get(CONF_SECRETKEY)
-    group_id = config.get(CONF_GROUP_ID)
-    port = config.get(CONF_PORT)
     camera_entity_id = config.get(CONF_CAMERA_ENTITY_ID)
+    group_list = config.get(CONF_GROUP_LIST)
+    liveness = config.get(CONF_LIVENESS)
+    name = config.get(CONF_NAME)
+    port = config.get(CONF_PORT)
     pic_url = config.get(CONF_PIC_URL)
+    secret_key = config.get(CONF_SECRETKEY)
     token = config.get(CONF_TOKEN)
 
-    add_devices([FaceSensor(name, port, camera_entity_id, api_key, secret_key, group_id, pic_url, token)])
+    add_devices([FaceSensor(api_key, camera_entity_id, group_list, liveness, name, port, pic_url, secret_key, token)])
 
 
 class FaceSensor(Entity):
 
-	savePath = ""
-	suffix = "tmp.jpg"
-	recognise = {}
-
-	def __init__(self, name, port, camera_entity_id, api_key, secret_key, group_id, pic_url, token):
-		self._name = name
-		self._state = False
-		self._port = str(port)
-		self._camera_entity_id = camera_entity_id
+	def __init__(self, api_key, camera_entity_id, group_list, liveness, name, port, pic_url, secret_key, token):
 		self._api_key = api_key
-		self._secret_key = secret_key
-		self._group_id = group_id
+		self._camera_entity_id = camera_entity_id
+		self._group_list = group_list
+		self._liveness = liveness
+		self._name = name
+		self._port = str(port)
 		self._pic_url = pic_url
+		self._secret_key = secret_key
 		self._token = token
+		self._state = False
+		self._save_path = ""
+		self._pic_name = ""
 		self.exists_path()
 
-		
 	@property
 	def name(self):
 		return self._name
-
 	
 	@property
 	def entity_picture(self):
 		if (self._state == True):
-			return '/local/images/' + FaceSensor.suffix
+			pic_path = "/local/images/" + self._pic_name
+			return pic_path
 		else:
 			return self._pic_url
 
-		
 	@property
 	def state(self):
 		return self._state
 	
-
 	@property
 	def device_state_attributes(self):
-		if (self._state == True):
-			if ("result" in FaceSensor.recognise):
-				return {
-					ATTR_SCORES : FaceSensor.recognise["result"][0]["scores"][0],
-					ATTR_UID : FaceSensor.recognise["result"][0]["uid"],
-					ATTR_GROUP_ID : FaceSensor.recognise["result"][0]["group_id"],
-					ATTR_USER_INFO : FaceSensor.recognise["result"][0]["user_info"],
-					ATTR_FACELIVENESS : FaceSensor.recognise["ext_info"]["faceliveness"]
-				}
-		return {
-			ATTR_SCORES : 'null',
-			ATTR_UID : 'null',
-			ATTR_GROUP_ID : 'null',
-			ATTR_USER_INFO : 'null',
-			ATTR_FACELIVENESS : 'null'
-		}
+		return ATTR_LIST
 
 	
 	def update(self):
-		self.download_picture()
-		ret = self.identify_face()
+		save_path = self._save_path + "tmp.jpg"
+		self.download_picture(save_path)
+		ret = self.face_searching(save_path)
 		if (ret == True):
 			self._state = True
 		else:
 			self._state = False
 
 			
-	def download_picture(self):
+	def download_picture(self, savePath):
 		""" download picture from homeassistant """
 		t = int(round(time.time()))
-		url = "http://127.0.0.1:%s/api/camera_proxy/%s?time=%d -o image.jpg"%(self._port, self._camera_entity_id, t)
-		headers = {'Authorization': "Bearer %s"%(self._token),
+		url = "http://127.0.0.1:{}/api/camera_proxy/{}?time={} -o image.jpg".format(self._port, self._camera_entity_id, t)
+		headers = {'Authorization': "Bearer {}".format(self._token),
 					'content-type': 'application/json'}
 		response = requests.get(url, headers=headers)
-		path = FaceSensor.savePath + FaceSensor.suffix
-		with open(path, 'wb') as fp:
+		with open(savePath, 'wb') as fp:
 		    fp.write(response.content)
-
 
 	def get_base64_file_content(self, filePath):
 	    with open(filePath, 'rb') as fp:
 	        return base64.b64encode(fp.read())
-
 
 	def get_token(self):
 		grant_type = 'client_credentials'
@@ -156,54 +149,30 @@ class FaceSensor(Entity):
 			return None
 
 
-	# def detect_face(self):
-	# 	from PIL import Image
-	# 	request_url = "https://aip.baidubce.com/rest/2.0/face/v2/detect"
-	# 	img = self.get_base64_file_content(FaceSensor.savePath)
-	# 	params = {
-	# 		"access_token" : self.get_token(),
-	# 	}
-	# 	data = {"image" : img}
-	# 	ret = requests.post(url=request_url, params=params, data=data)
-	# 	ret_json = json.loads(ret.text)
-	# 	if ("result" in ret_json):
-	# 		location = ret_json["result"][0]["location"]
-	# 		img = Image.open(FaceSensor.savePath)
-	# 		left = location["left"]
-	# 		upper = location["top"]
-	# 		middle = (location["width"] + location["height"]) / 2
-	# 		right = left + middle
-	# 		lower = upper + middle
-	# 		cropped = img.crop((left, upper, right, lower))
-	# 		cropped.save(FaceSensor.savePath)
-	# 		img = Image.open(FaceSensor.savePath)
-	# 		out = img.resize((48, 48),Image.ANTIALIAS)
-	# 		out.save(FaceSensor.iconPath)
-
-
-	def identify_face(self):
-		request_url = "https://aip.baidubce.com/rest/2.0/face/v2/identify"
-		pic_path = FaceSensor.savePath + FaceSensor.suffix
-		img = self.get_base64_file_content(pic_path)	
-		params = {'access_token' : self.get_token()}
-		data = {
-			'image' : img,
-			'group_id' : self._group_id,
-			'ext_fields' : 'faceliveness'
-			}
-		ret = requests.post(url=request_url, params=params, data=data)	
-		ret_json = json.loads(ret.text)
-		if ("result" in ret_json):
-			FaceSensor.recognise = ret_json
-			scores = ret_json["result"][0]["scores"][0]
-			faceliveness = float(ret_json["ext_info"]["faceliveness"])
-			FaceSensor.suffix = ret_json["result"][0]["uid"] + ".jpg"
-			if ((ret_json["result"][0]["scores"][0] > PASS_SCORE) and (faceliveness > THRESHOLD)):
+	def face_searching(self, picPath):
+		request_url = "https://aip.baidubce.com/rest/2.0/face/v3/search"
+		headers = {'Content-Type' : 'application/json'}
+		group_id_list = eval(self._group_list)
+		img = self.get_base64_file_content(picPath)
+		data = {'image_type' : 'BASE64',
+				 'image' : img,
+				 'access_token' : self.get_token(),
+				 'group_id_list' : group_id_list,
+				 'liveness_control' : self._liveness}
+		response = requests.post(url=request_url, headers=headers, data=data)
+		ret_json = json.loads(response.text)
+		for key in ATTR_LIST:
+			ATTR_LIST[key] = 'null'
+		if ret_json['result']:
+			score = ret_json['result']['user_list'][0]['score']
+			if (score > PASS_SCORE):
+				for key in ATTR_LIST:
+					ATTR_LIST[key] = ret_json['result']['user_list'][0][key]
+				self._pic_name = ATTR_LIST[ATTR_UID] + ".jpg"
+				save_path = self._save_path + self._pic_name
+				self.download_picture(save_path)
 				return True
-		else:
-			_LOGGER.error(ret.text)
-			return False
-
+		return False
 
 	def exists_path(self):
 		import os
@@ -213,9 +182,9 @@ class FaceSensor(Entity):
 			hassbian_path = hassbian_path + "/www/images/"
 			if not (os.path.exists(hassbian_path)):
 				os.makedirs(hassbian_path)
-			FaceSensor.savePath = hassbian_path
+			self._save_path = hassbian_path
 		elif (os.path.exists(docker_path)):
 			docker_path = docker_path + "/www/images/"
 			if not (os.path.exists(docker_path)):				
 				os.makedirs(docker_path)
-			FaceSensor.savePath = docker_path
+			self._save_path = docker_path
